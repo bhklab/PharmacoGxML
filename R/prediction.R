41201f81286a36d11d7eed8a57898ed721d0cf70
zhaleh-safikhani <zhalehsafikhani@gmail.com>
2018-03-15 16:32:02 -0400

    prediction

--- R/prediction.R
+++ R/prediction.R
@@ -1,14 +1,15 @@
 prediction <- function(model,
-                       method=c("ridge", "lasso", "random_forest", "svm"))
+                       method=c("ridge", "lasso", "random_forest", "svm"),
+                       test.set)
 {
   predicted_labels <- NULL
 
   if(method == "ridge" | method == "lasso" | method == "elastic_net"){
     features <- rownames(coef(model))[2:nrow(coef(model))]
-    predicted_labels <- cbind(predicted_labels, predict(model, newx=validation.set[,features], s="lambda.min"))
+    predicted_labels <- cbind(predicted_labels, predict(model, newx=test.set[,features], s="lambda.min"))
   }else{
     features <- colnames(model$trainingData)[1:ncol(model$trainingData) -1 ]
-    predicted_labels <- cbind(predicted_labels, predict(model, newdata=validation.set[,features]))
+    predicted_labels <- cbind(predicted_labels, predict(model, newdata=test.set[,features]))
   }
   return(predicted_labels)
 }
