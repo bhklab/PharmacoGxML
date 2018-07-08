246d0e244728f942a7d82c8e6bfa20032812fb4e
zhaleh-safikhani <zhalehsafikhani@gmail.com>
2018-07-08 11:19:49 -0400

    xlim updated

--- R/validation.R
+++ R/validation.R
@@ -5,7 +5,7 @@ validation <- function(model,
                        assessment=c("corr", "CI", "mCI"))
 {
   predicted_labels <- NULL
-  
+
   if(method == "ridge" | method == "lasso" | method == "elastic_net"){
     features <- rownames(coef(model))[2:nrow(coef(model))]
     predicted_labels <- cbind(predicted_labels, predict(model, newx=validation.set[,features], s="lambda.min"))
@@ -17,19 +17,20 @@ validation <- function(model,
   cells <- intersect(names(validation.labels), rownames(predicted_labels))
   validation.labels <- validation.labels[cells]
   predicted_labels <- predicted_labels[cells, 1]
-  
+
   plot(validation.labels,
        predicted_labels,
        main=sprintf("Validation\n%s\nmethod:%s", "lapatinib", method),
-       cex.main=1, ylab="Predictions", xlab="drug sensitivity", pch=20, col="gray40")
+       cex.main=1, ylab="Predictions", xlab="drug sensitivity", pch=20, col="gray40",
+       xlim=c(0, 1), ylim=c(0, 1))
   fit <- lm(predicted_labels ~ validation.labels)
   slope <- fit$coefficients[[2]]
   intercept <- fit$coefficients[[1]]
-  
+
   intercept <- round(intercept, 2)
   slope <- round(slope, 2)
   equation <- paste("y = ", slope, "x + ", intercept, sep = "")
-  
+
   legend_label <- NULL
   if(method == "ridge" | method == "lasso" | method == "elastic_net"){
     abline(intercept, slope, lty=2)
