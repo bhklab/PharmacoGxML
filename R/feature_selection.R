0e58e5db8832773f8108e9651b4112715e9e871d
Zhaleh Safikhani <zhalehsafikhani@gmail.com>
2018-10-26 16:00:09 -0400

    features

--- R/feature_selection.R
+++ R/feature_selection.R
@@ -3,7 +3,7 @@ featureSelection <- function(x, y, method, features.no){
          "mRMR"={
            #var_features <- apply(x, MARGIN=2, var)
            #features <- names(sort(var_features, decreasing=T))[1:(features.no*10)]
-           f_data <- mRMR.data(data=as.data.frame(cbind(x[,features], y), stringAsFactor=FALSE))
+           f_data <- mRMR.data(data=as.data.frame(cbind(x, y), stringAsFactor=FALSE))
            system.time(
            features <- mRMR.ensemble(data=f_data,
                                      target_indices=ncol(x) + 1,
